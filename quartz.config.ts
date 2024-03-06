586338aa2df8382457578c3db4cd9283ec8c7b57
Yeshwanth Chala <yeshwanthchala@Yeshwanths-Air.lan>
2024-03-05 21:35:57 -0700

    Quartz sync: Mar 5, 2024, 9:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -27,10 +27,10 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#fff3f0",
           lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
+          gray: "#6c757d",
+          darkgray: "#000000",
           dark: "#2b2b2b",
           secondary: "#284b63",
           tertiary: "#84a59d",
